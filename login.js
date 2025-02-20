cb17f118d0ba1b9b9af7b7d5884986bcd75869fd
Raweerot Bhasidhchirapiroch <raweerot.bha@student.mahidol.ac.th>
2025-02-20 16:35:06 +0700

    Modified login page

--- login.js
+++ login.js
@@ -2,12 +2,12 @@ function validateLogin() {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
 
-  if (username === "u6588132" && password === "Gones2545") {
+  if (username == "u6588132" && password == "Gones2545") {
     alert("Login successful! Redirecting...");
     window.location.href = "https://www.canva.com/design/DAGfP5q_5Gk/u9YzccuPxDx7DBGeS6ZNzA/view?utm_content=DAGfP5q_5Gk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h6f611c7cdf"; // Change to your actual page  
       return false;
   } else {
     alert("Incorrect information, try again.");
-      return false;
+      return true;
   }
 }
