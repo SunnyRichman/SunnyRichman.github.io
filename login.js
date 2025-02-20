22f50ea8d527235ac355bb1075619c474b57c2e8
Raweerot Bhasidhchirapiroch <raweerot.bha@student.mahidol.ac.th>
2025-02-20 20:46:21 +0700

    Modified Password

--- login.js
+++ login.js
@@ -2,13 +2,13 @@ function validateLogin() {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
 
-  if (username == "u6588132" && password == "Gones2545") {
+  if (username == "hci_paper" && password == "hci_paper") {
     alert("Login successful! Redirecting...");
     window.location.href = "https://www.canva.com/design/DAGfP5q_5Gk/u9YzccuPxDx7DBGeS6ZNzA/view?utm_content=DAGfP5q_5Gk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h6f611c7cdf"; // Change to your actual page  
       return false;
   } else {
     alert("Incorrect information, try again.");
-    window.location.href = "https://sunnyrichman.github.io/login.html"; // Change to your actual page  
+    window.location.href = "https://sunnyrichman.github.io/login.html";
       return false;
   }
 }
