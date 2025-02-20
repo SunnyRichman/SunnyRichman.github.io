d626e30c5c13f2d812c5cfd7ea4fecda7ece11ed
Raweerot Bhasidhchirapiroch <raweerot.bha@student.mahidol.ac.th>
2025-02-20 23:00:29 +0700

    Updated the destination page

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ function validateLogin() {
 
   if (username == "hci_paper" && password == "hci_paper") {
     alert("Login successful! Redirecting...");
-    window.location.href = "https://www.canva.com/design/DAGfP5q_5Gk/u9YzccuPxDx7DBGeS6ZNzA/view?utm_content=DAGfP5q_5Gk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h6f611c7cdf"; // Change to your actual page  
+    window.location.href = "https://www.canva.com/design/DAGfd30AaHc/L-lOV8nWC6v62yKvxk8tLw/view?utm_content=DAGfd30AaHc&utm_campaign=share_your_design&utm_medium=link2&utm_source=shareyourdesignpanel";
       return false;
   } else {
     alert("Incorrect information, try again.");
