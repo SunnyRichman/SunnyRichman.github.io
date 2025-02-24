58f74cca70424b76616579af052d93667667aec5
Raweerot Bhasidhchirapiroch <raweerot.bha@student.mahidol.ac.th>
2025-02-24 22:48:19 +0700

    Assigned Link

--- login.js
+++ login.js
@@ -2,9 +2,9 @@ function validateLogin() {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
 
-  if (username == "hci_paper" && password == "hci_paper") {
+  if (username == "sw_design" && password == "sw_design") {
     alert("Login successful! Redirecting...");
-    window.location.href = "url";
+    window.location.href = "https://www.canva.com/design/DAGf2dsBXgo/zUKwOhHCRB15juZuj93tlg/view?utm_content=DAGf2dsBXgo&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h5b1dd2eafd";
       return false;
   } else {
     alert("Incorrect information, try again.");
