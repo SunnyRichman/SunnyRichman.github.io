c272520094121a1db1392d887ec7b4c2f60965f0
Raweerot Bhasidhchirapiroch <raweerot.bha@student.mahidol.ac.th>
2025-07-12 23:46:36 +0700

    Modified pdf renderer

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3966754727"
+    serviceWorkerVersion: "2399269827"
   }
 });
